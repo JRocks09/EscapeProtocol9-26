93c58ae82a7f12204d8550f1e77652ef8e71e5d7
JRocks9 <josephrizzuto0@gmail.com>
2024-09-28 17:01:17 -0400

    Improved Box Pushing

--- Escape_Protocol_/Assets/Scripts/ObjectPull.cs
+++ Escape_Protocol_/Assets/Scripts/ObjectPull.cs
@@ -4,38 +4,61 @@ using UnityEngine;
 
 public class ObjectPull : MonoBehaviour
 {
-public float defaultMass;
-public float imovableMass;
-public bool beingPushed;
-float xPos;
+	public float defaultMass;
+	public float imovableMass;
+	public bool beingPushed;
+	float xPos;
 
-public Vector3 lastPos;
+	public Vector3 lastPos;
+	
+	public int mode;
+	public int colliding;
 
-public int mode;
-public int colliding;
+	Rigidbody2D rb;
+	
 	// Use this for initialization
-void Start () {
-	xPos = transform.position.x;
-	lastPos = transform.position;
-	}
+	void Start ()
+	{
+		xPos = transform.position.x;
+		lastPos = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+		beingPushed = false;
+    }
 	
 	// Update is called once per frame
-void FixedUpdate () {
-	if (mode == 0) {
-	if (beingPushed == false) {
-		transform.position = new Vector3 (xPos, transform.position.y);
-	} else
-		xPos = transform.position.x;
-	} else if (mode == 1) {
+	void FixedUpdate ()	
+	{
+		if (mode == 0)
+		{
+			if (beingPushed == false)
+			{
+				transform.position = new Vector3(xPos, transform.position.y);
+				rb.constraints = 
+					RigidbodyConstraints2D.FreezePositionX |
+					RigidbodyConstraints2D.FreezeRotation;
+				print("not pushing");
+			}
+			else
+			{
+				xPos = transform.position.x;
+				rb.constraints = RigidbodyConstraints2D.None;
+                print("pushing");
 
-	if (beingPushed == false) {
-		GetComponent<Rigidbody2D> ().mass=imovableMass;
-	} else {
-		GetComponent<Rigidbody2D> ().mass=defaultMass;
-		//	GetComponent<Rigidbody2D> ().isKinematic = false;
+            }
 		}
-					
-	}
+
+		/*else if (mode == 1)
+		{
+			if (beingPushed == false)
+			{
+				rb.mass = imovableMass;
+			}
+			else
+			{
+				rb.mass = defaultMass;
+				//	GetComponent<Rigidbody2D> ().isKinematic = false;
+			}
+		}*/
 	}
 
 }
--- Escape_Protocol_/Assets/Scripts/PlayerPushPull.cs
+++ Escape_Protocol_/Assets/Scripts/PlayerPushPull.cs
@@ -5,32 +5,37 @@ using UnityEngine;
 public class PlayerPushPull : MonoBehaviour
 {
 
-public float distance=1f;
-public LayerMask boxMask;
+	public float distance = 0.5f;
+	public LayerMask boxMask;
 
-GameObject HeavyObject;
+	GameObject HeavyObject;
 
 	// Use this for initialization
-void Start () {
-	
-}
-	
-	// Update is called once per frame
-void Update () {
-	Physics2D.queriesStartInColliders = false;
-	RaycastHit2D hit= Physics2D.Raycast (transform.position, Vector2.right * transform.localScale.x, distance, boxMask);
-	
-	if (hit.collider != null && hit.collider.gameObject.tag == "Box" && Input.GetKeyDown (KeyCode.P)) {
-		HeavyObject = hit.collider.gameObject;
-		HeavyObject.GetComponent<FixedJoint2D> ().connectedBody = this.GetComponent<Rigidbody2D> ();
-		HeavyObject.GetComponent<FixedJoint2D> ().enabled = true;
-		HeavyObject.GetComponent<ObjectPull> ().beingPushed = true;
-	} else if (Input.GetKeyUp (KeyCode.P)) {
-		HeavyObject.GetComponent<FixedJoint2D> ().enabled = false;
-		HeavyObject.GetComponent<ObjectPull> ().beingPushed = false;
-	}
+	void Start () 
+	{
 
 	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Physics2D.queriesStartInColliders = false;
+		RaycastHit2D hit= Physics2D.Raycast (transform.position, Vector2.right * transform.localScale.x, distance, boxMask);
+	
+		if (hit.collider != null && hit.collider.gameObject.tag == "Box" && Input.GetKeyDown (KeyCode.P)) 
+		{
+			HeavyObject = hit.collider.gameObject;
+			HeavyObject.GetComponent<FixedJoint2D> ().connectedBody = GetComponent<Rigidbody2D> ();
+			HeavyObject.GetComponent<FixedJoint2D> ().enabled = true;
+			HeavyObject.GetComponent<ObjectPull> ().beingPushed = true;
+		} 
+		else if (Input.GetKeyUp (KeyCode.P)) 
+		{
+			HeavyObject.GetComponent<FixedJoint2D> ().enabled = false;
+			HeavyObject.GetComponent<ObjectPull> ().beingPushed = false;
+		}
+
+    }
 
 
 void OnDrawGizmos()

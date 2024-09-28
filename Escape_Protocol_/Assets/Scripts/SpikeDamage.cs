6ef14009529bd0136f04f3d7ed66f8e9914738a0
JRocks9 <josephrizzuto0@gmail.com>
2024-09-28 01:59:58 -0400

    Isaiah's Branch Merges
    Copied over Isaiah's changes and added I-frames (it needs some work though)

--- Escape_Protocol_/Assets/Scripts/SpikeDamage.cs
+++ Escape_Protocol_/Assets/Scripts/SpikeDamage.cs
@@ -6,6 +6,8 @@ public class SpikeDamage : MonoBehaviour
 {
     // Takes the current player's health
     public playerHealth currentPlayerHealth;
+    public float invincibilityTimer = 3;
+    private bool playerInvincible;
 
     // Damage
     public float damage;
@@ -13,7 +15,7 @@ public class SpikeDamage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerInvincible = false;
     }
 
     // Update is called once per frame
@@ -25,11 +27,18 @@ public class SpikeDamage : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other)
     {
         // If the player collides with the object
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && !playerInvincible)
         {
             // Takes away health from the player based on the damage value
             // currentPlayerHealth.health -= damage;
             other.gameObject.GetComponent<playerHealth>().health -= damage;
+            Invoke(nameof(InvincibilityOver), invincibilityTimer);
+            playerInvincible = true;
         }
+    }
+
+    void InvincibilityOver()
+    {
+        playerInvincible = false;
     }
 }
